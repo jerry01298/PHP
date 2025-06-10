e25d877fb80e862cd8e3d7b5655ef111b1831a40
jerry01298 <86393308+jerry01298@users.noreply.github.com>
2025-06-10 09:55:12 +0800

    Update 20.user_edit.php

--- 20.user_edit.php
+++ 20.user_edit.php
@@ -14,9 +14,11 @@
     // 執行更新密碼SQL指令
         if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){
             echo "修改錯誤";
+    // 資料已更新，3秒後自動返回使用者列表頁
             echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
         }else{
             echo "修改成功，三秒鐘後回到網頁";
+    // 資料已更新，3秒後自動返回使用者列表頁
             echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
         }
     }
