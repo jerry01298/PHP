4666d159131faeb21bc589e28004c313555fa7ec
jerry01298 <86393308+jerry01298@users.noreply.github.com>
2025-06-10 09:53:25 +0800

    Update 27.bulletin_edit.php

--- 27.bulletin_edit.php
+++ 27.bulletin_edit.php
@@ -11,7 +11,7 @@
     else{
     //建立資料庫  
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-    //執行SQL更新語句(但未做安全處理)
+    //執行SQL更新語句
         if (!mysqli_query($conn, "update bulletin set title='{$_POST['title']}',content='{$_POST['content']}',time='{$_POST['time']}',type={$_POST['type']} where bid='{$_POST['bid']}'")){
             echo "修改錯誤";
             echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
